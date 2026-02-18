ccf6cd4328059ae3d591f7aeacf467f13292dddc
Luan Fabrício <luanlimaleite@hotmail.com>
2026-02-18 01:49:04 -0300

    refactor(dynamic array): moves da macros to dynamic_array.h

---
+++ include/dynamic_array.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <stdlib.h>
+
+#define da_create(type)\
+	struct {\
+		type *items;\
+		size_t size, capacity;\
+	}
+
+#define da_append(arr, x)\
+	do {\
+		if ((arr)->items == NULL || (arr)->capacity <= 0){\
+			free((arr)->items);\
+			(arr)->size = 0;\
+			(arr)->capacity = 5;\
+			(arr)->items = malloc(sizeof(x) * (arr)->capacity);\
+		} else if (((arr)->size + 1) >= (arr)->capacity) {\
+				(arr)->capacity *= 2;\
+				(arr)->items = realloc((arr)->items, sizeof(*(arr)->items) * (arr)->capacity);\
+		}\
+		(arr)->items[(arr)->size] = x;\
+		(arr)->size += 1;\
+	} while(0)
+
+#define da_remove(arr, i)\
+	do {\
+		size_t size = (arr)->size;\
+		if (i < size) {\
+			if (size > 0 ) {\
+				size--;\
+				(arr)->size = size;\
+			}\
+			(arr)->items[i] = (arr)->items[size];\
+		}\
+	} while(0)
+
+#define da_for(arr) for (size_t i = 0; i < (arr)->size; i++)
+
+#define da_for_each(arr, type)\
+	for (\
+		struct { size_t i; type* item; } loop = { 0, &(arr)->items[0] };\
+		loop.i < (arr)->size;\
+		loop.i++, loop.item = &(arr)->items[loop.i]\
+	)
--- include/general.h
+++ include/general.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <stdlib.h>
-
 #include "raylib.h"
+
+#include "dynamic_array.h"
 
 typedef struct {
 	Vector3 start, end;
@@ -20,32 +20,16 @@ typedef struct {
 	Color color;
 } cube_t;
 
-#define da_create(type)\
-	struct {\
-		type *items;\
-		size_t size, capacity;\
-	}
+typedef struct {
+	line_t line;
+	double death_time;
+} ray_t;
 
 typedef struct {
+	Camera camera;
+
 	da_create(line_t) lines;
 	da_create(sphere_t) spheres;
 	da_create(cube_t) cubes;
+	da_create(ray_t) rays;
 } game_t;
-
-#define da_append(arr, x)\
-	do {\
-		if ((arr)->items == NULL || (arr)->capacity <= 0){\
-			free((arr)->items);\
-			(arr)->size = 0;\
-			(arr)->capacity = 5;\
-			(arr)->items = malloc(sizeof(x) * (arr)->capacity);\
-		} else if (((arr)->size + 1) >= (arr)->capacity) {\
-				(arr)->capacity *= 2;\
-				(arr)->items = realloc((arr)->items, sizeof(*(arr)->items) * (arr)->capacity);\
-		}\
-		(arr)->items[(arr)->size] = x;\
-		(arr)->size += 1;\
-	} while(0)
-
-#define da_for_each(arr, type)\
-	for (struct { size_t i; type* item; } loop = { 0, &(arr)->items[0] } ; loop.i < (arr)->size; loop.i++, loop.item = &(arr)->items[loop.i])
