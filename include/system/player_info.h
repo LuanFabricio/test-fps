801e480d05ef72943a05e3a3e6d5d63a6ef001d7
Luan Fabrício <luanlimaleite@hotmail.com>
2026-03-01 22:58:28 -0300

    fix(player info): fixes level property type and renames upgrade function

--- include/system/player_info.h
+++ include/system/player_info.h
@@ -3,13 +3,13 @@
 typedef struct {
 	attributes_t attributes;
 
-	float
-		level,
-		current_xp,
+	float current_xp,
 		next_level_xp;
 
-	int32_t attribute_points, total_attribute_points;
+	int32_t level,
+		attribute_points,
+		total_attribute_points;
 } player_info_t;
 
 void player_info_gain_xp(player_info_t* entity, const float xp);
-void attributes_spend_skill_upgrade(attributes_t* increment_attributes, const attributes_e attribute);
+void player_info_spend_skill_upgrade(player_info_t* info, const attributes_e attribute);
